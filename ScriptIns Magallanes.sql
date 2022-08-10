927d0e4e0c33bbd103de164e0b57c1dca2388735
RocioM-dev <68021863+RocioM-dev@users.noreply.github.com>
2022-08-09 22:04:05 -0300

    Add files via upload

---
+++ ScriptIns Magallanes.sql
@@ -0,0 +1,205 @@
+USE base_rocio; 
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	39091447	,	1	,	'Rocío'	,	'Celeste'	,	'Magallanes'	,	'Ovidio Lagos'	,	426	,	'Centro'	,	'2000'	,	'Rosario'	,	'Argentina'	,	2 ,	'rociocmagallanes@gmail.com'	,	25/07/1995	,	27	);						
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	14144293	,	2	,	'Patricia'	,	'Ana '	,	'Gómez'	,	'Naciones Unidas'	,	14	,	'Naciones Unidas'	,	'5700'	,	'San Luis '	,	'Argentina'	,	3	,	'patricia-ana-19@hotmail.com'	,	19/01/1960	,	62	);				
+COMMIT;		
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	2748596	,	3	,	'Mirtha'	,	'Elsa'	,	'Suarez'	,	'Sucre'	,	145	,	'Centro'	,	'5700'	,	'San Luis '	,	'Argentina'	,	4	,	'mirtha@gmail.com'	,	22/10/1935	,	87	);													
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	40568978	,	4	,	'Francisco'	,	'Emiliano'	,	'Magallanes'	,	'Buenos Aires'	,	456	,	'Centro sur'	,	'5700'	,	'San Luis'	,	'Argentina'	,	5	,	'franmagallanes@gmail.com'	,	10/05/2022	,	24	);								
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	40568978	,	4	,	'Francisco'	,	'Emiliano'	,	'Magallanes'	,	'Buenos Aires'	,	456	,	'Centro sur'	,	'5700'	,	'San Luis'	,	'Argentina'	,	5	,	'franmagallanes@gmail.com'	,	10/05/2022	,	24	);										
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	41735948	,	5	,	'Victoria'	,	'Belén'	,	'Verdura'	,	'Galindo'	,	2415	,	'Centro'	,	'2134'	,	'Funes'	,	'Argentina'	,	6	,	'verduravictoria@gmail.com'	,	10/02/1999	,	23	);								
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	37854623	,	6	,	'Manuel'	,	null	,	'Citta Giordano'	,	'Santa Fe'	,	2415	,	'Centro'	,	'2000'	,	'Rosario'	,	'Argentina'	,	7	,	'mcg@gmail.com'	,	03/12/1994	,	27	);									
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	36456781	,	7	,	'Gloria'	,	'Estefania'	,	'Magallanes'	,	'Ovidio Lagos'	,	426	,	'Centro'	,	'2000'	,	'Rosario'	,	'Argentina'	,	8	,	'gmagallanes@gmail.com'	,	10/04/1990	,	32	);									
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	24568985	,	8	,	'Franco'	,	null	,	'Sanchez'	,	'Santa Fe'	,	1000	,	'Centro'	,	'2000'	,	'Rosario'	,	'Argentina'	,	9	,	'fsanchez@gmail.com'	,	05/05/1980	,	42	);
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	19548726	,	9	,	'María'	,	'Belén'	,	'Gisbert'	,	'Santa Fe'	,	2878	,	'Alberto Olmedo'	,	'2000'	,	'Rosario'	,	'Argentina'	,	10	,	'mgisbert@gmail.com'	,	02/09/1970	,	52	);
+COMMIT;	
+
+INSERT INTO personas (	nr_documento 	,	nr_secuencia 	,	ds_nombre 	,	ds_segundo_nombre 	,	ds_apellido	,	ds_calle 	,	nr_calle 	,	ds_barrio 	,	nr_codigo_postal	,	ds_ciudad 	,	ds_pais 	,	nr_telefono 	,	ds_email 	,	dt_nacimiento 	,	nr_edad 	) 
+VALUES (	35045869	,	10	,	'Victoria'	,	'María'	,	'Ruiz'	,	'24 de septiembre'	,	1859	,	'General Paz'	,	'5004'	,	'Córdoba capital'	,	'Argentina'	,	11	,	'ruiz@gmail.com'	,	29/07/1985	,	37	)										;
+COMMIT;	
+
+select* from personas;
+
+INSERT INTO enfermeros (	nr_secuencia 	,	nr_enfermero 	,	ds_profesion 	,	nr_matricula_p 	,	ds_provincia 	,	dt_matricula 	)																												
+VALUES (	10	,	1	,	 'Enfermería'	,	2478	,	 'San Luis'	,	01/01/2022	)								;
+COMMIT;	
+
+INSERT INTO enfermeros (	nr_secuencia 	,	nr_enfermero 	,	ds_profesion 	,	nr_matricula_p 	,	ds_provincia 	,	dt_matricula 	)																												
+VALUES 	(	9	,	2	,	 'Enfermería'	,	5247	,	 'Córdoba'	,	01/01/2020	)																																			;
+COMMIT;	
+
+INSERT INTO enfermeros (	nr_secuencia 	,	nr_enfermero 	,	ds_profesion 	,	nr_matricula_p 	,	ds_provincia 	,	dt_matricula 	)																												
+VALUES (	8	,	3	,	 'Enfermería'	,	3654	,	 'Santa Fe'	,	01/01/2013	)																															;
+COMMIT;	
+
+select* from enfermeros;
+
+INSERT INTO medicos (	nr_secuencia 	,	nr_medico 	,	ds_profesion 	,	nr_matricula_p	,	ds_provincia 	,	dt_matricula 	,	ds_especialidad	,	nr_reg_esp	)																											
+VALUES 	(	7	,	1	,	 'Medicina'	,	4125	,	 'San Luis'	,	02/03/2021	,	 'Clínica Médica'	,	220	) ;																																																					;
+COMMIT;	
+
+INSERT INTO medicos (	nr_secuencia 	,	nr_medico 	,	ds_profesion 	,	nr_matricula_p	,	ds_provincia 	,	dt_matricula 	,	ds_especialidad	,	nr_reg_esp	)																											
+VALUES (	6	,	2	,	 'Medicina'	,	7584	,	 'Córdoba'	,	03/05/2019	,	 'Cardiología'	,	214	) ;																																																			;
+COMMIT;	
+
+INSERT INTO medicos (	nr_secuencia 	,	nr_medico 	,	ds_profesion 	,	nr_matricula_p	,	ds_provincia 	,	dt_matricula 	,	ds_especialidad	,	nr_reg_esp	)																											
+VALUES (	5	,	3	,	 'Medicina'	,	3621	,	 'Santa Fe'	,	20/11/2020	,	 'Dermatología'	,	523	);																																																						;
+COMMIT;	
+
+INSERT INTO medicos (	nr_secuencia 	,	nr_medico 	,	ds_profesion 	,	nr_matricula_p	,	ds_provincia 	,	dt_matricula 	,	ds_especialidad	,	nr_reg_esp	)																											
+VALUES 	(	4	,	4	,	 'Medicina'	,	2222	,	 'Santa Fe'	,	25/07/2017	,	 'Infectóloga'	,	586	)	;																																																				;
+COMMIT;	
+
+select* from medicos
+
+INSERT INTO procedimientos (	nr_procedimientos 	,	ds_proc 	,	tp_item 	,	ie_situacion 	)
+VALUES (	420101	,	 'Aspiración de secreciones'	,	1	,	 1	);
+COMMIT;
+
+
+INSERT INTO procedimientos (	nr_procedimientos 	,	ds_proc 	,	tp_item 	,	ie_situacion 	)
+VALUES (	170101	,	 'Oxígeno por cánula'	,	1	,	 1	); 
+COMMIT;
+
+INSERT INTO procedimientos (	nr_procedimientos 	,	ds_proc 	,	tp_item 	,	ie_situacion 	)
+VALUES (	340101	,	 'Toma de signos vitales'	,	1	,	 1	);
+COMMIT;
+
+INSERT INTO procedimientos (	nr_procedimientos 	,	ds_proc 	,	tp_item 	,	ie_situacion 	)
+VALUES (	340102	,	 'Colocar paciente a 45 grados'	,	1	,	 1	);
+COMMIT;
+
+INSERT INTO procedimientos (	nr_procedimientos 	,	ds_proc 	,	tp_item 	,	ie_situacion 	)
+VALUES (	340105	,	 'Control de oximetría de pulso' 	,	1	,	 0	);
+COMMIT;
+
+SELECT* FROM PROCEDIMIENTOS; 
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	25456	,	 'Dexametasona 4mg/ml'	,	2	,	1	) ;
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	24568	,	 'Dipirona 1gr amp /2ml'	,	2	,	1	);
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	23587	,	 'Paracetamol 1000 mg'	,	2	,	1	) ;
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	26547	,	 'Salbutamol 20 ml'	,	2	,	0	) ;
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	21458	,	 'Solución fisiológica 500ml'	,	2	,	0	);
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	12457	,	 'Mascarilla de reservorio'	,	3	,	0	);
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	13589	,	 'Vendas 20cm'	,	3	,	1	);
+COMMIT;
+
+INSERT INTO mat_medic (	nr_med_mat 	,	ds_med_mat 	,	tp_item 	,	ie_situacion 	)
+VALUES (	16589	,	 'Bisturí N°4' 	,	3	,	1	) ;
+COMMIT;
+
+select* from mat_medic ;
+
+INSERT INTO prescr_medicas(	nr_secuencia_prescr , nr_medico_prescr 	,	nr_sec_paciente 	,	nr_med_mat	,	nr_procedimiento	,	dt_prescr	)
+VALUES (	1	,	1	,	3	,	25456	,	null	,	01/08/2022	),
+(	2	,	1	,	3	,	24568	,	null	,	01/08/2022	),
+(	3	,	1	,	3	,	23587	,	null	,	01/08/2022	),
+(	4	,	2	,	2	,	24568	,	null	,	03/08/2022	),
+(	5	,	3	,	2	,	25456	,	null	,	03/08/2022	),
+(	6	,	4	,	1	,	null	,	420101	,	09/08/2022	),
+(	7	,	4	,	1	,	null	,	170101	,	09/09/2022	),
+(	8	,	4	,	1	,	null	,	340101	,	09/10/2022	),
+(	9	,	4	,	1	,	null	,	340102	,	09/11/2022	),
+(	10	,	4	,	1	,	null	,	340105	,	09/12/2022	);
+COMMIT; 
+
+SELECT* FROM PRESCR_MEDICAS;
+
+INSERT INTO items_adm (	nr_sec_adm 	,	nr_sec_prescr	,	nr_enfer_ejecutor 	,	tp_item 	,	dt_administracion 	,	ie_adm	,	nr_item	)
+VALUES (	1	,	1	,	1	,	2	,	 '2022-08-01'	,	1	,	25456	),
+(	2	,	3	,	1	,	2	,	 '2022-08-01'	,	1	,	23587	),
+(	3	,	2	,	1	,	2	,	 '2022-08-01'	,	1	,	24568	),
+(	4	,	5	,	2	,	2	,	 '2022-08-03'	,	1	,	25456	),
+(	5	,	4	,	2	,	2	,	 '2022-08-03'	,	1	,	24568	),
+(	6	,	10	,	3	,	1	,	 '2022-08-09'	,	1	,	340105	),
+(	7	,	9	,	3	,	1	,	 '2022-08-09'	,	1	,	340102	),
+(	8	,	8	,	3	,	1	,	 '2022-08-09'	,	1	,	340101	),
+(	9	,	7	,	3	,	1	,	 '2022-08-09'	,	1	,	170101	),
+(	10	,	6	,	3	,	1	,	 '2022-08-09'	,	1	,	420101	);
+COMMIT;
+
+SELECT*FROM ITEMS_ADM;
+
+SELECT*FROM PERSONAS;
+UPDATE personas  SET dt_nacimiento='1995-07-25' where nr_secuencia=1;
+UPDATE personas SET dt_nacimiento='1960-01-19' where nr_secuencia=2;
+UPDATE personas SET dt_nacimiento='1935-10-22' where nr_secuencia=3;
+UPDATE personas SET dt_nacimiento='1998-05-10' where nr_secuencia=4;
+UPDATE personas SET dt_nacimiento='1999-02-10' where nr_secuencia=5;
+UPDATE personas SET dt_nacimiento='1994-12-03' where nr_secuencia=6;
+UPDATE personas SET dt_nacimiento='1990-04-10' where nr_secuencia=7;
+UPDATE personas SET dt_nacimiento='1980-05-05' where nr_secuencia=8;
+UPDATE personas SET dt_nacimiento='1970-09-02' where nr_secuencia=9;
+UPDATE personas SET dt_nacimiento='1985-07-29' where nr_secuencia=10;
+
+select* from enfermeros;
+UPDATE enfermeros SET dt_matricula='2022-01-01' where nr_enfermero=1;
+UPDATE enfermeros SET dt_matricula='2020-01-01' where nr_enfermero=2;
+UPDATE enfermeros SET dt_matricula='2003-01-01' where nr_enfermero=3;
+
+select* from medicos;
+UPDATE medicos SET dt_matricula='2022-03-02' where nr_medico=1;
+UPDATE medicos SET dt_matricula='2020-05-03' where nr_medico=2;
+UPDATE medicos SET dt_matricula='2003-11-20' where nr_medico=3;
+UPDATE medicos SET dt_matricula='2003-07-25' where nr_medico=4;
+
+UPDATE prescr_medicas SET dt_prescr='2022-08-01' where nr_secuencia_prescr=1;
+UPDATE prescr_medicas SET dt_prescr='2022-08-01' where nr_secuencia_prescr=2;
+UPDATE prescr_medicas SET dt_prescr='2022-08-01' where nr_secuencia_prescr=3;
+UPDATE prescr_medicas SET dt_prescr='2022-08-03' where nr_secuencia_prescr=4;
+UPDATE prescr_medicas SET dt_prescr='2022-08-03' where nr_secuencia_prescr=5;
+UPDATE prescr_medicas SET dt_prescr='2022-08-09' where nr_secuencia_prescr=6;
+UPDATE prescr_medicas SET dt_prescr='2022-08-09' where nr_secuencia_prescr=7;
+UPDATE prescr_medicas SET dt_prescr='2022-08-09' where nr_secuencia_prescr=8;
+UPDATE prescr_medicas SET dt_prescr='2022-08-09' where nr_secuencia_prescr=9;
+UPDATE prescr_medicas SET dt_prescr='2022-08-09' where nr_secuencia_prescr=10;
+
+ALTER TABLE items_adm MODIFY dt_administracion DATETIME;
+ALTER TABLE personas MODIFY dt_nacimiento DATE;
+ALTER TABLE enfermeros MODIFY dt_matricula DATE;
+ALTER TABLE medicos MODIFY dt_matricula DATE;
+
